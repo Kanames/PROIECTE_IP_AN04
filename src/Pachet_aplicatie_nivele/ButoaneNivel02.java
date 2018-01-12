4cfbd05ab8be78e0b8630ffbe790035d981b85eb
Kanames <Kanames72@gmail.com>
2018-01-12 11:46:55 +0200

    Update ButoaneNivel02.java

--- src/Pachet_aplicatie_nivele/ButoaneNivel02.java
+++ src/Pachet_aplicatie_nivele/ButoaneNivel02.java
@@ -5,7 +5,7 @@ public class ButoaneNivel02 implements IButoaneNivel02{
     public ArrayList<Button> butoaneNivel02;
     
     @Override
-    public IIteratorNivel02 getIterator(int numberOfButtonsPerRow){
+    public IteratorButoane getIterator(int numberOfButtonsPerRow){
         butoaneNivel02 = new ArrayList<Button>();
         for(){
           //Button btn1 = new ButtonNivel02();
@@ -14,22 +14,4 @@ public class ButoaneNivel02 implements IButoaneNivel02{
         }
         return new IteratorButoane();
     }
-    
-    private class IteratorButoane implements IIteratorNivel02{
-      int index;
-      
-      @Override
-      public boolean hasNext(){
-          if(index < butoaneNivel02.size()){
-              return true;
-          }
-         return false;
-      }
-      
-      @Override
-       public Object next(){
-          if(this.hasNext()){
-              return butoaneNive02.iterator();
-          }
-       }
 }
