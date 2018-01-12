0848f064adc51aaeb9c08879a8b09af1c2a8237c
Kanames <Kanames72@gmail.com>
2018-01-12 11:47:14 +0200

    Update IButoaneNivel02.java

--- src/Pachet_aplicatie_nivele/IButoaneNivel02.java
+++ src/Pachet_aplicatie_nivele/IButoaneNivel02.java
@@ -1,5 +1,5 @@
 package Pachet_aplicatie_main
 
 public interface IButoaneNivel02{
-    public IIteratorNivel02 getIterator(int numberOfButtonsPerRow)
+    public IteratorButoane getIterator(int numberOfButtonsPerRow)
 }
