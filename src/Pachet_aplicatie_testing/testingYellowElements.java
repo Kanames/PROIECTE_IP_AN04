da6307e8eeb789c22716ada0fd183a49fddc405a
kanames <kanames72@gmail.com>
2018-01-15 13:05:59 +0200

    testing JUnit

--- src/Pachet_aplicatie_testing/testingYellowElements.java
+++ src/Pachet_aplicatie_testing/testingYellowElements.java
@@ -1,29 +1,31 @@
 package Pachet_aplicatie_testing;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Logger;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import Pachet_aplicatie_data.GameHelperPosition;
 import Pachet_aplicatie_main.ContextAplicatie;
+import Pachet_infoScene.IObserver;
+import Pachet_infoScene.ISubject;
+import Pachet_infoScene.SituatieJocObserver;
 import Pachet_nivel_02.ButoaneNivel02;
-import Pachet_nivel_02.ButtonNivel02Implementare;
-import Pachet_nivel_02.IteratorButoane;
-import javafx.scene.Node;
-import javafx.scene.layout.Pane;
+import Pachet_nivel_02.IButoaneNivel02;
+import Pachet_nivel_03.Cadru;
+import javafx.scene.shape.Rectangle;
 
-class testingYellowElements {
+public class testingYellowElements {
 	private static final int NR_OF_ROWS = 2;
 	private static final int NR_OF_BTNS_PER_ROW = 10;
+	
 	private static Logger logger = Logger.getAnonymousLogger();
 	
 	// Categorie Sabloane creationale - sablon SINGLETON
 	//------------------------------------------ Testarea sablonului SINGLETON
 	@Test
-	void testSingletonGood() {
+	public void testSingletonGood() {
 		logger.info("<<<Testing GOOD singleton>>>");
 		ContextAplicatie contextAPP;
 		ContextAplicatie contextAPP2;
@@ -38,7 +40,7 @@ class testingYellowElements {
 		
 	}
 	@Test
-	void testSingletonBad() {
+	public void testSingletonBad() {
 		logger.info("<<<Testing BAD singleton>>>");
 		ContextAplicatie contextAPP;
 		ContextAplicatie contextAPP2;
@@ -52,89 +54,117 @@ class testingYellowElements {
 		assertEquals(contextAPP,contextAPP2);
 	}
 	//------------------------------------------ Testarea sablonului SINGLETON
-	
-	// Categorie Sabloane creationale - sablon FACTORY
-	//------------------------------------------ Testarea sablonului FACTORY
-//	@Test
-//	void testFactoryGood() {
-//		//Check to see the object is the correct concrete implementation you were looking for:
-//		//You can check if the factory setup the concrete instances with valid instance variables.
-//		Scene tipScenaDorit = null;
-//		String tipScena = "Intro";
-//		if(tipScena.equalsIgnoreCase("Intro")) {
-//			System.out.println("<< Intrat in IF case good(Intro)");
-//			tipScenaDorit = Build_IntroAPP.setting(null);
-//			System.out.println("<< Iesit din IF(Intro)");
-//		}
-//		Boolean b = tipScenaDorit instanceof Scene;
-//	}
-//	
-//	@Test
-//	void testFactoryBad() {
-//			
-//	}
-	//------------------------------------------ Testarea sablonului FACTORY
-		
-	
-	
-	// Categorie Sabloane creationale - sablon FACTORY
-	//------------------------------------------ Testarea sablonului ITERATOR
-	@Test
-	void testIteratorGood() {
-		int nrObiecte = 0 ;
-		IteratorButoane iterator = getIterator();
-		while(iterator.hasNext()){	
-			Node node = (Node) iterator.next();
-			logger.info("...obiectul cu nr: " + (nrObiecte++) +" adaugat prin iteratie.");
-		}
-	}
-	
-	public IteratorButoane getIterator(){
-		ArrayList<ButtonNivel02Implementare> butoaneNivel02 = new ArrayList<ButtonNivel02Implementare>();
-        ArrayList<String> pozitieXsiY = GameHelperPosition.calcularePozitiNivel02(800,400,NR_OF_ROWS,NR_OF_BTNS_PER_ROW);
-        System.out.println("pozitieXsiY: "+pozitieXsiY);
-		for(String pozitieXsiYStr : pozitieXsiY){
-			String[] pozXY = pozitieXsiYStr.split("\\s+");
-				int x = Integer.valueOf(pozXY[0]);
-				int y = Integer.valueOf(pozXY[1]);
-				System.out.println("Button creat ! X="+x+" Y="+y);
-				int fillWidth   = Integer.valueOf(pozXY[2]);;
-				int fillHeight  = Integer.valueOf(pozXY[3]);;
-				ButtonNivel02Implementare btn = new ButtonNivel02Implementare(x ,y,fillWidth,fillHeight);
-				butoaneNivel02.add(btn);
-		}
-        
-        return new IteratorButoane();
-    }
 
-	//------------------------------------------ Testarea sablonului ITERATOR
+	//------------------------------------------------------------------------------
+	@Test
+	public void testIteratorObject() throws Exception{
+	  logger.info("<<<Testing iterator object>>>");
+	  ButoaneNivel02 btnGenerator = new ButoaneNivel02();
+	  btnGenerator.setNumberOfRows(NR_OF_ROWS);
+	  btnGenerator.setNumberButtonsPerRow(NR_OF_BTNS_PER_ROW);
+	  if(btnGenerator instanceof IButoaneNivel02){
+	    logger.info("btnGenerator istanceof IButoaneNivel02 -> TRUE");
+	  }
+	}
+	//------------------------------------------------------------------------------
+
+	//------------------------------------------------------------------------------
+	@Test
+	public void testObserverObject() throws Exception{
+	  logger.info("<<<Testing observer object>>>");
+	  logger.info("Crearea de 3 obiecte de tipul SituatieJocObserver");
+	  SituatieJocObserver situatieJocObserver1 = new SituatieJocObserver(new ISubject(){
+
+		@Override
+		public void register(SituatieJocObserver situatieJocObserver) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void unregister(SituatieJocObserver o) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void notifyObserver() {
+			// TODO Auto-generated method stub
+			
+		}});
+	  SituatieJocObserver situatieJocObserver2 = new SituatieJocObserver(new ISubject(){
+
+		@Override
+		public void register(SituatieJocObserver situatieJocObserver) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void unregister(SituatieJocObserver o) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void notifyObserver() {
+			// TODO Auto-generated method stub
+			
+		}});
+	  SituatieJocObserver situatieJocObserver3 = new SituatieJocObserver(new ISubject(){
+
+		@Override
+		public void register(SituatieJocObserver situatieJocObserver) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void unregister(SituatieJocObserver o) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void notifyObserver() {
+			// TODO Auto-generated method stub
+			
+		}});
+	  if(situatieJocObserver1 instanceof IObserver){
+	    logger.info("situatieJocObserver1 istanceof IObserver -> TRUE");
+	  }
+	}
+	//------------------------------------------------------------------------------
+
+	//------------------------------------------------------------------------------
+		@Test
+		public void testCompositeObject() throws Exception{
+		  logger.info("<<<Testing composite object>>>");
+		  int cadreCerute = 2;
+		  Cadru cadru = new Cadru(cadreCerute);
+		  List<Rectangle> listaCadre = cadru.getCadre();
+		  for(int i = 0 ; i <listaCadre.size() ; i++){
+			  if(listaCadre.get(i) instanceof Rectangle){
+				    logger.info("cadru instanceof Rectangle -> TRUE");
+			  }
+		  }
+		  
+		}
+	//------------------------------------------------------------------------------
 	
+	//------------------------------------------------------------------------------
+	@Test
+	public void testNrObiecteCerutePrimiteComposite() throws Exception{
+	  logger.info("<<<Testing composite object>>>");
+	  int cadreCerute = 2;
+	  Cadru cadru = new Cadru(cadreCerute);
+	  List<Rectangle> listaCadre = cadru.getCadre();
+	  if(listaCadre.size() == cadreCerute){
+	    logger.info("Lista de cadre cerute si primite sunt egale !");
+	  }
+	}
+	//------------------------------------------------------------------------------
+
 	
-	
-	
-	
-	
-	
-	
-	
-	
-//------- Testarea sablonului SINGLETON---------	Ex internet !
-//	private ClassicSingleton sone = null, stwo = null;
-//	   private static Logger logger = Logger.getRootLogger();
-//	   public SingletonTest(String name) {
-//	      super(name);
-//	   }
-//	   public void setUp() {
-//	      logger.info("getting singleton...");
-//	      sone = ClassicSingleton.getInstance();
-//	      logger.info("...got singleton: " + sone);
-//	      logger.info("getting singleton...");
-//	      stwo = ClassicSingleton.getInstance();
-//	      logger.info("...got singleton: " + stwo);
-//	   }
-//	   public void testUnique() {
-//	      logger.info("checking singletons for equality");
-//	      Assert.assertEquals(true, sone == stwo);
-//	   }
-//------- Testarea sablonului SINGLETON--------- https://www.javaworld.com/article/2073352/core-java/simply-singleton.html
+
 }
